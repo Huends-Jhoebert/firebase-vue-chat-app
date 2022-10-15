e732ff76d63cd749a0c66b95266d93fd7c3fe5b6
Jhoebert Huenda <huendahuenda20@gmail.com>
2022-10-15 19:56:07 +0800

    Added ability to swtich beteen signup and login

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="welcome container">
-    <p>welcome</p>
-    <SignupForm />
+    <div v-if="showLogin">
+      <h2>Login</h2>
+      <LoginForm />
+      <p>
+        No account yet?
+        <span @click="showLogin = false">Signup</span> instead.
+      </p>
+    </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignupForm />
+      <p>
+        Already registered?
+        <span @click="showLogin = true">Login</span> instead.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
 import SignupForm from "../components/SignupForm.vue";
+import LoginForm from "../components/LoginForm.vue";
+import { ref } from "@vue/reactivity";
 
 export default {
-  components: { SignupForm },
+  components: { SignupForm, LoginForm },
+  setup() {
+    const showLogin = ref(true);
+
+    return { showLogin };
+  },
 };
 </script>
 
